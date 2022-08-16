f2f8adcedb82d29bb4fb62081554661e5a3110fa
Gabriel Bomtempo <gabrielbomlima@gmail.com>
2022-08-15 23:21:27 -0300

    homepage-done

--- data/posts.php
+++ data/posts.php
@@ -13,21 +13,21 @@
             'title' => 'Semi electric trucks',
             'description' => 'Elon Musk announced that the Tesla Semi electric truck with 500 miles of range is going to start shipping this year.',
             'tags' => ['electric cars', 'tecnology', 'innovation'],
-            'img' => 'img2.jpg',
+            'img' => 'img2.jfif',
         ],
         [
             'id' => 3,
             'title' => 'Samsung Sees Foldable as the Future. Will Customers?',
             'description' => 'Samsung sees foldable technology as the next step and has been leading the charge with its Galaxy Z Flip and Z Fold.',
             'tags' => ['cell_phones', 'tecnology'],
-            'img' => 'img3.jpg',
+            'img' => 'img3.jfif',
         ],
         [
             'id' => 4,
             'title' => 'Japan\'s largest robot exhibition',
             'description' => 'An exhibition of robot chefs in Japan, tracking technology through walls, augmented reality lenses and chip implantation without opening the skull.',
             'tags' => ['robot', 'innovation', 'tecnology'],
-            'img' => 'img4.jpg',
+            'img' => 'img4.jfif',
         ],
     ]
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
     include_once("templates/header.php");
-    print_r($posts);
+    // print_r($posts);
 
 ?>
 
